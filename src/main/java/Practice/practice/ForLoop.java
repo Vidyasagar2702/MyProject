88be281566b7c2d615636d7d30d2b3ef6164e3a4
vidyasagar <vidyasagar2702@gmail.com>
2019-06-22 23:42:33 +0530

    forloop reverse printing

--- src/main/java/Practice/practice/ForLoop.java
+++ src/main/java/Practice/practice/ForLoop.java
@@ -2,12 +2,19 @@ package Practice.practice;
 
 public class ForLoop {
 
+	
 	public static void main(String[] args) {
 
 		for(int i=1;i<=13;i++)
 		{
 			System.out.println(i);
 		}
+		
+		for(int i=13;i>=0;i--)
+		{
+			System.out.println(i);
+		}
+
 
 	}
 
