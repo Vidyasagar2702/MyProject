a88267bd461eb3f24b634a3528b69b403cf57548
vidyasagar <vidyasagar2702@gmail.com>
2019-06-23 22:32:13 +0530

    while loop changes

---
+++ src/main/java/Practice/practice/WhileLoop.java
@@ -0,0 +1,18 @@
+package Practice.practice;
+
+public class WhileLoop {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=10;
+		while(i>0)
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+
+}
