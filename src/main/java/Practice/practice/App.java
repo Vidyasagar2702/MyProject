7862c333fc1cda617e79dc67595c400bd1f5e784
vidyasagar <vidyasagar2702@gmail.com>
2019-07-12 19:23:50 +0530

    excel read write file added

--- src/main/java/Practice/practice/App.java
+++ src/main/java/Practice/practice/App.java
@@ -1,5 +1,7 @@
 package Practice.practice;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        
+    	
+    	String str1 = "madam";
+		String []str = str1.split(str1);
+    	StringBuilder str2=null;
+    	int count =str.length;
+    	for(int i=count-1;i>=0;i--)
+    	{
+    		System.out.println(str[i]);
+    	}
+    	//System.out.println(str2);
+        if (str1.equals(str2))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not a palidrome");
+        }
+     
+
+        
     }
 }
